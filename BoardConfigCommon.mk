d461a954f459d7ed356fd218321f46c0dba2b1e1
Steve Kondik <shade@chemlab.org>
2013-08-09 04:42:11 -0700

    msm8660: Enable TARGET_DISPLAY_INSECURE_MM_HEAP
     * Ancient userspace :(
     * Also remove the OLD_ION flag, it's not necessary at all.
    
    Change-Id: I2d42d11c927784727e94c19d7c7177e9b71e3563

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,5 +91,6 @@ TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 # To handle radio version checks
 TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/msm8660-common
 
-# Use older ION API for qcom/media
-TARGET_HAS_OLD_QCOM_ION := true
+# Use legacy MM heap behavior
+TARGET_DISPLAY_INSECURE_MM_HEAP := true
+
